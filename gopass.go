4fa76e46f96b4381c66216b8057d47b81c1d99f9
srbdev <sylvain.rb@gmail.com>
2022-04-08 23:10:48 -0600

    Implement gopass

--- gopass.go
+++ gopass.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+func generateTokens() (string, string, string) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	tokens := [3]string{}
+
+	for i := range tokens {
+		token := make([]byte, 6)
+		for j := range token {
+			r, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+			if err != nil {
+				fmt.Print(err)
+			}
+
+			token[j] = letters[r.Uint64()]
+		}
+
+		tokens[i] = string(token)
+	}
+
+	return tokens[0], tokens[1], tokens[2]
+}
+
+func generate() string {
+	f, s, t := generateTokens()
+
+	return fmt.Sprintf("%s-%s-%s", f, s, t)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println(generate())
 }
