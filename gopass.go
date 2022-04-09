a405b2c5600739f7a5f5ff74680345d33d391a23
srbdev <sylvain.rb@gmail.com>
2022-04-09 12:13:15 -0600

    Improve code organization

--- gopass.go
+++ gopass.go
@@ -6,31 +6,33 @@ import (
 	"math/big"
 )
 
-func generateTokens() (string, string, string) {
+func generateToken() []byte {
 	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	tokens := [3]string{}
+	token := make([]byte, 6)
 
-	for i := range tokens {
-		token := make([]byte, 6)
-		for j := range token {
-			r, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
-			if err != nil {
-				fmt.Print(err)
-			}
-
-			token[j] = letters[r.Uint64()]
+	for j := range token {
+		r, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			fmt.Print(err)
 		}
+		token[j] = letters[r.Uint64()]
+	}
 
-		tokens[i] = string(token)
+	return token
+}
+
+func generateTokens() (string, string, string) {
+	tokens := [3]string{}
+	for i := range tokens {
+		tokens[i] = string(generateToken())
 	}
 
 	return tokens[0], tokens[1], tokens[2]
 }
 
 func generate() string {
-	f, s, t := generateTokens()
-
-	return fmt.Sprintf("%s-%s-%s", f, s, t)
+	first, second, third := generateTokens()
+	return fmt.Sprintf("%s-%s-%s", first, second, third)
 }
 
 func main() {
