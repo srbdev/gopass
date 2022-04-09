72d3fc476eff1be70898f263994e0343cb7e8095
srbdev <sylvain.rb@gmail.com>
2022-04-09 12:29:46 -0600

    Remove return character from printed output

--- gopass.go
+++ gopass.go
@@ -6,10 +6,10 @@ import (
 	"math/big"
 )
 
-func generateToken() []byte {
-	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	token := make([]byte, 6)
+func generateToken(length int) []byte {
+	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+	token := make([]byte, length)
 	for j := range token {
 		r, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
 		if err != nil {
@@ -21,20 +21,18 @@ func generateToken() []byte {
 	return token
 }
 
-func generateTokens() (string, string, string) {
-	tokens := [3]string{}
+func generateTokens(n int) []string {
+	tokenLength := 6
+
+	tokens := make([]string, n)
 	for i := range tokens {
-		tokens[i] = string(generateToken())
+		tokens[i] = string(generateToken(tokenLength))
 	}
 
-	return tokens[0], tokens[1], tokens[2]
-}
-
-func generate() string {
-	first, second, third := generateTokens()
-	return fmt.Sprintf("%s-%s-%s", first, second, third)
+	return tokens
 }
 
 func main() {
-	fmt.Println(generate())
+	tokens := generateTokens(3)
+	fmt.Printf("%s-%s-%s", tokens[0], tokens[1], tokens[2])
 }
